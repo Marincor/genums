ef86b46b907290d4ca3be843b1b82622e453190c
Marincor <gabriel.marinho@nuvidio.com.br>
2025-11-11 09:12:43 -0300

    feat: implement first version of enum

--- examples/strings/strings.go
+++ examples/strings/strings.go
@@ -6,9 +6,27 @@ import (
 	"github.com/marincor/genums"
 )
 
-var Color = genums.New("Color", "red", "green", "blue")
+type Color genums.EnumString[Color]
+type Color2 genums.EnumString[Color2]
+
+var colorFactory = genums.NewFactory[Color]()
+var colorFactory2 = genums.NewFactory[Color2]()
+
+var (
+	Red   = colorFactory.Add("red")
+	Black = colorFactory.Add("black")
+
+	Red2 = colorFactory2.Add("red")
+)
+
+func foo(color Color) {
+	fmt.Println(color)
+}
 
 func main() {
-	fmt.Println(Color.Name)
-	fmt.Println(Color.Value)
+	foo(Red)
+	foo(Black)
+
+	// error cannot use Red2 (type Color2) as type Color in argument to foo
+	// foo(Red2)
 }
--- genums.go
+++ genums.go
@@ -1,13 +1,41 @@
 package genums
 
-type enum struct {
-	Name  string
-	Value []string
+type baseEnum[T any] interface {
+	IsEqual(provider T) bool
+	// MarshalJSON returns the JSON representation of the enum.
+	MarshalJSON() ([]byte, error)
+	Add(value string) baseEnum[T]
 }
 
-func New(name string, values ...string) *enum {
-	return &enum{
-		Name:  name,
-		Value: values,
+type EnumString[T baseEnum[T]] interface {
+	baseEnum[T]
+}
+
+func NewFactory[T any]() baseEnum[T] {
+	var newInstance EnumStringImpl[T]
+
+	return newInstance
+
+}
+
+type EnumStringImpl[T any] string
+
+func (e EnumStringImpl[T]) MarshalJSON() ([]byte, error) {
+	return []byte("\"" + string(e) + "\""), nil
+}
+
+func (e EnumStringImpl[T]) Raw() string {
+	return string(e)
+}
+
+func (e EnumStringImpl[T]) IsEqual(rawValue T) bool {
+	if any(rawValue) == nil {
+		return false
 	}
+
+	return e.Raw() == any(rawValue)
+}
+
+func (e EnumStringImpl[T]) Add(value string) baseEnum[T] {
+	return EnumStringImpl[T](value)
 }
